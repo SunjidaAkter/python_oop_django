21ca51a38862e41943d1a2976d9972844fd070d7
SunjidaAkter <sanjidabintesayid7@gmail.com>
2024-02-07 09:13:35 +0600

    mission accomplished 🔥🚀🔥

---
+++ OOPandPythonProgramming/introduction_to_python/module_2/built_in_function.py
@@ -0,0 +1,9 @@
+
+#* docs.python.org/3/library/functions.html
+highest=max(45,78,67,45,89)#for tuple
+highest=max([45,78,67,45,89])#for list
+print(highest)
+print(len([34,34]))
+# print(len(34,34))#it doesnt work for tuple
+# print(sum([34,34]))#it doesnt work for tuple
+print(sum([34,34]))
