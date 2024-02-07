10d209a5045d356a79dbdb4bfdeada22d8e17a67
SunjidaAkter <sanjidabintesayid7@gmail.com>
2024-02-07 10:39:21 +0600

    mission accomplished 🔥🚀🔥

--- OOPandPythonProgramming/introduction_to_python/module_2/stl_of_list.py
+++ OOPandPythonProgramming/introduction_to_python/module_2/stl_of_list.py
@@ -1,2 +1,13 @@
 
 #* docs.python.org/3/tutorial/datastructures.html
+numbers=[12,45,98,68]
+numbers.append(56)#for pushing back
+print(numbers)
+numbers.insert(2,71)#for pushing in an index
+print(numbers)
+numbers.remove(98)#work as erase
+print(numbers)
+last=numbers.pop()#work as pop_back and back together
+print(last,numbers)
+idx=numbers.index(71)#work as pop_back and back together
+print(idx)
--- OOPandPythonProgramming/introduction_to_python/module_2/tempCodeRunnerFile.py
+++ OOPandPythonProgramming/introduction_to_python/module_2/tempCodeRunnerFile.py
@@ -1,1 +1,9 @@
-45,78,67,45,89
+
+#* docs.python.org/3/tutorial/datastructures.html
+numbers=[12,45,98,68]
+numbers.append(56)#for pushing back
+print(numbers)
+numbers.insert(2,71)#for pushing in an index
+print(numbers)
+numbers.remove(98)#work as erase
+print(numbers)
