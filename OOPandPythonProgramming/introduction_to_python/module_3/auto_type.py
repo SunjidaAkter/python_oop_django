8bcdf807d0e36252fd84c2c58d4525e157ce684e
SunjidaAkter <sanjidabintesayid7@gmail.com>
2024-02-08 20:18:18 +0600

    mission accomplished 🔥🚀🔥

---
+++ OOPandPythonProgramming/introduction_to_python/module_3/auto_type.py
@@ -0,0 +1,6 @@
+import pyautogui
+from time import *
+sleep(5)
+pyautogui.write('Hi, How are you?',interval=0.25)
+pyautogui.press('enter')
+
---
+++ OOPandPythonProgramming/introduction_to_python/module_3/cam.py
@@ -0,0 +1,8 @@
+
+#! was not worked
+import cv2
+cam =cv2.VideoCapture(0)
+while True:
+    _,frame=cam.read()
+    cv2.imshow('my cam',frame)
+    cv2.waitkey(1)
