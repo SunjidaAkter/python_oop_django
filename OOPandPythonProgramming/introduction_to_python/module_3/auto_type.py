a07fca58fa892ed7299f8d5623b48619b5dc6d15
SunjidaAkter <sanjidabintesayid7@gmail.com>
2024-02-09 17:50:53 +0600

    mission accomplished 🔥🚀🔥

--- OOPandPythonProgramming/introduction_to_python/module_3/auto_type.py
+++ OOPandPythonProgramming/introduction_to_python/module_3/auto_type.py
@@ -1,8 +1,7 @@
 import pyautogui
 from time import *
-sleep(3)
-pyautogui.write('Hi, How are you?',interval=0.05)
+# sleep(3)
+pyautogui.write('Hi, How are you?',interval=0.15)
 pyautogui.press('enter')
-# Hi, How are you?
-
+# # *Hi, How are you?
 
