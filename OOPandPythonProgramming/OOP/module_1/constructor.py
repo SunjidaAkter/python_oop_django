09cc4ff23c661a1c24c85ccb9765cfbc43193dae
SunjidaAkter <sanjidabintesayid7@gmail.com>
2024-02-10 16:52:17 +0600

    mission accomplished 🔥🚀🔥

--- OOPandPythonProgramming/OOP/module_1/constructor.py
+++ OOPandPythonProgramming/OOP/module_1/constructor.py
@@ -1,3 +1,12 @@
+
+"""
+In object-oriented programming, a method is a function associated with an object. It defines the behavior of an object and is called on an instance of the class.
+
+A constructor is a special method used for initializing object attributes. In Python, it is named __init__. It is called automatically when an object is created.
+
+An instance is an individual occurrence of an object created from a class.
+"""
+
 class Phone:
     manufactured='China'
 
@@ -14,3 +23,14 @@ my_phone=Phone('kala','oppo',98000)
 print(my_phone.owner,my_phone.brand,my_phone.price)
 her_phone=Phone('dhala','poop',89000)
 print(her_phone.owner,her_phone.brand,her_phone.price)
+
+"""
+Instance: 
+?my_phone is an instance of the Phone class, representing a specific phone object.
+
+Constructor: 
+?The __init__ method is the constructor. It initializes the attributes (owner, brand, price) when a new Phone object is created.
+
+Method: 
+?send_sms is a method of the Phone class. It can be called on an instance (e.g., my_phone.send_sms('123456789', 'Hello')) to perform actions related to the phone, like sending an SMS.
+"""
