9a823f9b9adc52042f17cb70ef50c12add69f478
SunjidaAkter <sanjidabintesayid7@gmail.com>
2024-02-10 17:01:26 +0600

    mission accomplished 🔥🚀🔥

---
+++ OOPandPythonProgramming/OOP/module_1/different_types_of_method.py
@@ -0,0 +1,7 @@
+"""
+There are three types of class :
+1. class method : a class method is going to affect the actual class 
+2. instance method : a instance method is going to affect the actual instance meaning that we can refer to self and the variables inside the instance
+3. static method : a static method might or might not be related to the class, it can be used outside or inside and it doesnt have anything rely on from the class itself
+
+"""
