f37e2049209c8a18058795766e10ab7dbe0c3767
SunjidaAkter <sanjidabintesayid7@gmail.com>
2024-02-11 23:41:29 +0600

    mission accomplished 🔥🚀🔥

--- OOPandPythonProgramming/OOP/module_2/fleet_management.py
+++ OOPandPythonProgramming/OOP/module_2/fleet_management.py
@@ -8,15 +8,20 @@ class Company:
         self.counters=[]
         self.managers=[]
         self.supervisors=[]
+        self.fare=[]
 class Driver:
-    def __init__(self,name,license,age) -> None:
+    def __init__(self,name,age,license):
         self.license=license        
-        self.name=name
-        self.age=age
-        
-        
-        
-        
-        license=license        
-        self.license=license        
+        self.name=name        
+        self.age=age        
+class Counter:
+    def __init__(self) -> None:
+        pass        
+    def purchase_ticket(self,start,destination):
+        pass        
+class Passanger:
+    pass
+class Supervisor:
+    pass
 
+red_mia=Driver('a','123',23)
