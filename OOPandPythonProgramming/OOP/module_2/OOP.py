88b136587caac51e06eaa608ad263fc3f7d98995
SunjidaAkter <sanjidabintesayid7@gmail.com>
2024-02-11 23:25:19 +0600

    mission accomplished 🔥🚀🔥

---
+++ OOPandPythonProgramming/OOP/module_2/OOP.py
@@ -0,0 +1,6 @@
+"""
+todo: Object-oriented programming is a programming paradigm that is based on the concept of "objects", which can contain data and code that manipulates that data.
+todo: In OOP, objects are created from templates called "classes", which define the properties and behavior of the objects they create.
+*The Four pillars of OOPs, abstraction, encapsulation, inheritance, and polymorphism, are integral to understanding and using OOP.
+*By using these four pillars of OOPs, developers can create programs that are powerful, maintainable, and extensible, making them a great choice for software development.
+"""
---
+++ OOPandPythonProgramming/OOP/module_2/fleet_management.py
@@ -0,0 +1,22 @@
+# Ena poribohon
+class Company:
+    def __init__(self,name,address):
+        self.name=name
+        self.bus=[]
+        self.routes=[]
+        self.drivers=[]
+        self.counters=[]
+        self.managers=[]
+        self.supervisors=[]
+class Driver:
+    def __init__(self,name,license,age) -> None:
+        self.license=license        
+        self.name=name
+        self.age=age
+        
+        
+        
+        
+        license=license        
+        self.license=license        
+
