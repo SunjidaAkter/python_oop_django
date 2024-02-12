66abf8bf72d9e0030dba82b8219634ac34e52f65
SunjidaAkter <sanjidabintesayid7@gmail.com>
2024-02-12 22:47:09 +0600

    mission accomplished 🔥🚀🔥

--- OOPandPythonProgramming/OOP/module_6.5/shop_and_product.py
+++ OOPandPythonProgramming/OOP/module_6.5/shop_and_product.py
@@ -5,23 +5,34 @@ class Shop:
         self.name=name
 
     def add_product(self,item):
-        self.shop_lst.append(item)    
+        self.shop_lst.append(item)
+
+    def buy_product(self,name,quantity):
+        for i in self.shop_lst:
+            if i['name']==name:
+                if i['quantity']<quantity:
+                    print('not enough')
+                else:
+                    i['quantity']-=quantity
+                    print('success',i['quantity'])    
+
 
 class Product:
 
-    def __init__(self,brand,price,color,quantity) :
-        self.brand=brand
+    def __init__(self,name,price,color,quantity) :
+        self.name=name
         self.price=price
         self.color=color
         self.quantity=quantity
 
     def get_product(self):
-        product={'brand':self.brand,'price':self.price,'color':self.color,'quantity':self.quantity}
+        product={'name':self.name,'price':self.price,'color':self.color,'quantity':self.quantity}
         return product
-bag=Product('gucci',12,'red',5)
-shoe=Product('apex',23,'blue',8)
+bag=Product('bag',12,'red',5)
+shoe=Product('shoe',23,'blue',8)
 
 swapna=Shop('swapna')
 swapna.add_product(bag.get_product())
 swapna.add_product(shoe.get_product())
 print(swapna.shop_lst)
+swapna.buy_product('bag',1)
