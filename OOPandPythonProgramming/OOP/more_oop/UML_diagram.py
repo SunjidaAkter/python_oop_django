e9d859e1657b4927b8e70ca745d4f3f48ee3f80b
SunjidaAkter <sanjidabintesayid7@gmail.com>
2024-02-15 20:30:24 +0600

    mission accomplished 🔥🚀🔥

---
+++ OOPandPythonProgramming/OOP/more_oop/UML_diagram.py
@@ -0,0 +1,4 @@
+
+#* A UML diagram is a way to visualize systems and software using Unified Modeling Language (UML).
+#* Software engineers create UML diagrams to understand the designs, code architecture, and proposed implementation of complex software systems. 
+#* UML diagrams are also used to model workflows and business processes.
