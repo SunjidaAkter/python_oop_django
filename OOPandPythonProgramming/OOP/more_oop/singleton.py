6c2fe78ec0c97b05554efcead42ac00b1cd61e48
SunjidaAkter <sanjidabintesayid7@gmail.com>
2024-02-16 10:08:48 +0600

    mission accomplished 🔥🚀🔥

--- OOPandPythonProgramming/OOP/more_oop/singleton.py
+++ OOPandPythonProgramming/OOP/more_oop/singleton.py
@@ -2,6 +2,16 @@
 #* singleton --> one single instance
 #* if you want a new instance, you will get the old one (already created ) instance
 class Singleton:
-    _instance=None
+    __instance=None
     def __init__(self) -> None:
-        if Singleton._instance is None
+        if Singleton.__instance is None:
+            Singleton.__instance=self
+        else:
+            raise Exception('This is singleton. already have an instance, use that one by calling get_instance method')    
+        
+    def get_instance():
+        if Singleton.__instance is None:
+            Singleton()
+        return Singleton.__instance
+first = Singleton.get_instance()
+print(first)
