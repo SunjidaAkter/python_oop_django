0b8ecc070dcf7603e6115c26d57e8a6d04e76803
SunjidaAkter <sanjidabintesayid7@gmail.com>
2024-02-21 13:33:57 +0600

    mission accomplished 🔥🚀🔥

---
+++ OOPandPythonProgramming/OOP_Project/school_oop/concepts.py
@@ -0,0 +1,20 @@
+"""
+-----------------------
+   HIHG LEVEL AREAS
+-----------------------
+School system
+1.student management
+2.teacher management
+3.cafeteria management
+4.sports management
+5.club management
+6.event management
+7.library management
+8.asset management
+-----------------------
+Core functionalties
+1. there will be a classroom consisting of multiple students
+2. student can enroll multiple classes
+3. teacher will take classes, they will take exams, they grade the exams
+4. system will generate the overall grade
+"""
