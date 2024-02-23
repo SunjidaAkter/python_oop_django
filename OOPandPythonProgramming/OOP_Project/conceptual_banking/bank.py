522fc2f708e709477a6f4bb1366354922db43f49
SunjidaAkter <sanjidabintesayid7@gmail.com>
2024-02-23 19:12:13 +0600

    mission accomplished 🔥🚀🔥

---
+++ OOPandPythonProgramming/OOP_Project/conceptual_banking/bank.py
@@ -0,0 +1,32 @@
+class Account:
+    accounts=[]
+
+    def __init__(self,name,accNumber,password,type):
+        self.name=name
+        self.accNo=accNumber
+        self.passw=password
+        self.type=type
+
+        self.balance=0
+
+        Account.accounts.append(self)
+
+    def deposit(self,amount):
+        if amount>=0:
+            self.balance+=amount
+        else:
+            print("\nInvalid Amount\n")    
+
+    def withdraw(self,amount):
+        if amount>=0:
+            self.balance-=amount
+        else:
+            print("\nInvalid Amount\n")    
+
+    def changeInfo(self,name):
+        self.name=name
+
+    # over loading of method changeInfo
+    def changInfo(self,name,password): 
+        self.name=name   
+        self.password=password   
