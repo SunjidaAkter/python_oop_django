800543b891e67388f9b5841d02db440f1f56261d
SunjidaAkter <sanjidabintesayid7@gmail.com>
2024-02-23 10:33:10 +0600

    mission accomplished 🔥🚀🔥

--- OOPandPythonProgramming/OOP_Project/school_oop/relationships.py
+++ OOPandPythonProgramming/OOP_Project/school_oop/relationships.py
@@ -1,13 +1,16 @@
 """
 ----------------------------------------------------------------------------
 Association in OOP means that objects of two or more classes can have some 
-connection between each other. Aggregation and composition are special cases 
+connection between each other
+(car && engine)(aggregatin)||(car && passangers)(composition)). 
+Aggregation and composition are special cases 
 of Association.
 ----------------------------------------------------------------------------
 1. Association
-example : doctor can be associated with multiple patients
+example : car can be associated with passangers or 
+          car can be associated with engine
 2. Aggregation
-example : classroom is a collection of students
+example : car can make a collections of its passangers
 3. Composition
-example : human composites different organs like heart,eyes, kidney
+example : engine is a composition of car
 """
