fecd21ba22b0a1061f5a62c17e79495908fc1b6a
SunjidaAkter <sanjidabintesayid7@gmail.com>
2024-02-28 19:54:37 +0600

    mission accomplished 🔥🚀🔥

--- IntroductionToDatabase/mysql_and_basic_query/module_details.py
+++ IntroductionToDatabase/mysql_and_basic_query/module_details.py
@@ -11,3 +11,22 @@
 #* set type 
 #* bit type
 # * for more type follow https://www.w3schools.com/mysql/mysql_datatypes.asp 
+
+# CREATE DATABASE practice;
+# USE practice;
+# CREATE TABLE Student(
+# 	Roll CHAR(4) PRIMARY KEY,
+#     Name VARCHAR(50),
+#     Marks DOUBLE
+# );
+# DROP TABLE Student;
+# INSERT INTO Student (Roll,Name) VALUES (2,'rahim');
+
+# SET SQL_SAFE_UPDATES=0;
+# SET SQL_SAFE_UPDATES=1;
+# UPDATE Student
+# SET Name='kaushik kumar paul'
+# WHERE Roll=1;
+
+# DELETE FROM Student
+# WHERE Roll=1;
