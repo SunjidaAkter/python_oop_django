d44d9cbf1864d905ab3e7644088ebc86bc34752b
SunjidaAkter <sanjidabintesayid7@gmail.com>
2024-02-28 17:06:08 +0600

    mission accomplished 🔥🚀🔥

--- IntroductionToDatabase/mysql_and_basic_query/module_details.py
+++ IntroductionToDatabase/mysql_and_basic_query/module_details.py
@@ -2,3 +2,12 @@
 #* query means khoja or search kora 
 #* query language means data k khojar jonno or process korar jonno j language use kora hoi 
 #* mysql password "password" 
+
+#* Data types-->
+#* numeric type 
+#* string type 
+#* date time type 
+#* binary type 
+#* set type 
+#* bit type
+# * for more type follow https://www.w3schools.com/mysql/mysql_datatypes.asp 
