ccadc8fd7093e7b95d47d3a9cb1e214cd1876b35
SunjidaAkter <sanjidabintesayid7@gmail.com>
2024-03-10 12:01:22 +0600

    mission accomplished 🔥🚀🔥

---
+++ DatabaseManagement/deep_dive_into_database/first_conceptual/theoretical_part.sql
@@ -0,0 +1,3 @@
+scalar subquery: returns a record with one raw and one column;
+multi line subquery: returns multiple record with multiple raw and column;
+co related subquery: in co related subquery there is a relation between inner(subquery) query and outer(main query) query;
