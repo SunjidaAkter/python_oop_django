735e13b1a434ebb128cc5b4fec7c99fd7c160251
SunjidaAkter <sanjidabintesayid7@gmail.com>
2024-03-25 23:07:31 +0600

    mission accomplished 🔥🚀🔥

---
+++ SoftwareDevelopmentProject/introduction_to_js/app.js
@@ -0,0 +1,6 @@
+const num1 = "100";
+const num2 = 100;
+
+console.log(num1 + num2); //type cast to string
+console.log(parseInt(num1) + num2); //avoiding type casting
+console.log(num1 * num2); //no type casting
