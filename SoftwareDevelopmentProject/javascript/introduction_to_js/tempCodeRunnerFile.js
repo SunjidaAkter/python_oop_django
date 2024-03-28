b7e9d7209457a88a7a86b73ba3cab89d6f9be424
SunjidaAkter <sanjidabintesayid7@gmail.com>
2024-03-28 22:41:35 +0600

    mission accomplished 🔥🚀🔥

--- SoftwareDevelopmentProject/javascript/introduction_to_js/tempCodeRunnerFile.js
+++ SoftwareDevelopmentProject/javascript/introduction_to_js/tempCodeRunnerFile.js
@@ -8,11 +8,3 @@ console.log(num1 * num2); //no type casting
 let numbers = [1, 2, 3, 3, 4, 4, 5, 6, 7, 8, 9, 10];
 let uniqueNumbers = [...new Set(numbers)];
 console.log(uniqueNumbers);
-//* for taking input
-//* install-> Node.js
-//* then, apply this-> npm install prompt-sync
-
-const prompt = require("prompt-sync")();
-
-let userInput = prompt("Please enter your name:");
-console.log("Hello, " + userInput);
