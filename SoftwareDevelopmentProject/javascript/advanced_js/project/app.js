53e1c713a04117b8d8cdc1a9ee97f3b7cc5f0206
SunjidaAkter <sanjidabintesayid7@gmail.com>
2024-03-30 11:21:37 +0600

    mission accomplished 🔥🚀🔥

--- SoftwareDevelopmentProject/javascript/advanced_js/project/app.js
+++ SoftwareDevelopmentProject/javascript/advanced_js/project/app.js
@@ -1,9 +1,28 @@
 const loadData = async () => {
+    const searchText = document.getElementById('search-box').value;
+    // console.log(searchText);
     try {
-        const res = await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?f=b`)
+        const res = await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?f=${searchText}`)
         const data = await res.json();
-        console.log(data.meals);
+        displayData(data.meals);
     } catch (e) {
         console.log(e);
     }
+}
+
+const displayData = (data) => {
+    document.getElementById("total-meals").innerText = data.length;
+    const mealsContainer = document.getElementById("meals-container");
+    data.forEach((item) => {
+        console.log(item);
+        const card = document.createElement("div");
+        card.classList.add("box");
+        card.innerHTML = `
+        <img class="box-img" src=${item.strMealThumb} alt="" />
+        <h2>${item?.strMeal}</h2>
+        <p>${item.strInstructions.slice(0, 50)}...</p>
+        <button>Details</button>
+        `//?. means optional chaining
+        mealsContainer.appendChild(card);
+    })
 }
