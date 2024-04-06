23264e224bbd7d30333d7d3f8a564d3decbaf6d9
SunjidaAkter <sanjidabintesayid7@gmail.com>
2024-04-06 18:03:55 +0600

    mission accomplished 🔥🚀🔥

--- SoftwareDevelopmentProject/django/introduction/intro.py
+++ SoftwareDevelopmentProject/django/introduction/intro.py
@@ -1,5 +1,5 @@
 # python -m venv .venv
 # .venv\Scripts\Activate
 # python -m pip install django
-# django-admin startproject django_project .
+# django-admin startproject django_project 
 # python manage.py startapp first_app
