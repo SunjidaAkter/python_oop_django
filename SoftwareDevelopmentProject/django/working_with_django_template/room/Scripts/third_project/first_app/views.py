acedef6521eb35042aea946da0dfca1d449598f8
SunjidaAkter <sanjidabintesayid7@gmail.com>
2024-04-06 10:58:47 +0600

    mission accomplished 🔥🚀🔥

---
+++ SoftwareDevelopmentProject/django/working_with_django_template/room/Scripts/third_project/first_app/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render
+import datetime
+# Create your views here.
+def home(request):
+    d={'author':'Rahim','age':20,'lst':['python','is','best'],
+       'birthDate':datetime.datetime.now(),'value':'','courses':[
+        {
+        'id':1,
+        'name':'python',
+        'fee':5000
+        },
+        {
+        'id':2,
+        'name':'django',
+        'fee':10000
+        },
+        {
+        'id':3,
+        'name':'flask',
+        'fee':20000
+        }
+    ]}
+    return render(request, 'home.html',d)
