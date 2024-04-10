369db40a0b2dbc734163493518fd8a6c7dea1167
SunjidaAkter <sanjidabintesayid7@gmail.com>
2024-04-10 15:45:03 +0600

    mission accomplished 🔥🚀🔥

--- SoftwareDevelopmentProject/django/more_files_and_templates/room/first_project/first_app/urls.py
+++ SoftwareDevelopmentProject/django/more_files_and_templates/room/first_project/first_app/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index),
+    path('', views.index,name='home'),
+    # path('about/<int:id>', views.about, name='about'),
+    # path('about/page/<int:id>', views.about, name='about'),
+    path('about/', views.about, name='about'),
 ]
