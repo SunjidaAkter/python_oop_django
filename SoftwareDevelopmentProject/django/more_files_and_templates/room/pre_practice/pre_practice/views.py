ee59b41b946bac9e8ba9f2af30631a4c888ef600
SunjidaAkter <sanjidabintesayid7@gmail.com>
2024-04-16 10:21:19 +0600

    mission accomplished 🔥🚀🔥

--- SoftwareDevelopmentProject/django/more_files_and_templates/room/pre_practice/pre_practice/views.py
+++ SoftwareDevelopmentProject/django/more_files_and_templates/room/pre_practice/pre_practice/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+import datetime
+
 def home(request):
-    return render(request, 'base.html')
+    d={'author':'Rahim','age':20,'lst':['python','is','best'],
+       'birthDate':datetime.datetime.now(),'value':'','val':5,'courses':[
+        {
+        'id':1,
+        'name':'python',
+        'fee':5000
+        },
+        {
+        'id':2,
+        'name':'django',
+        'fee':10000
+        },
+        {
+        'id':3,
+        'name':'flask',
+        'fee':20000
+        }
+    ]}
+    return render(request, 'base.html',d)
