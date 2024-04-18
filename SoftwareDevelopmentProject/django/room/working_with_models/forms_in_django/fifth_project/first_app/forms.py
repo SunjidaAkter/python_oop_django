12c8619232d24265a2916c49cfc4570204db2967
SunjidaAkter <sanjidabintesayid7@gmail.com>
2024-04-18 19:36:47 +0600

    mission accomplished 🔥🚀🔥

--- SoftwareDevelopmentProject/django/room/working_with_models/forms_in_django/fifth_project/first_app/forms.py
+++ SoftwareDevelopmentProject/django/room/working_with_models/forms_in_django/fifth_project/first_app/forms.py
@@ -1,16 +1,18 @@
 from django import forms#form api
 
+# Widgets==field to html input
 class contactForm(forms.Form):
-    name=forms.CharField(label="User name")
+    name=forms.CharField(label="Full Name : ", initial="Sunjida",help_text="Enter your name",required=True,disabled=False,widget=forms.Textarea (attrs={'id':'text_area','class':'class1 class2','placeholder':'Enter your full name'}))
     file=forms.FileField()
-    # email=forms.EmailField(label="User email")
+    email=forms.EmailField(label="User email")
+    age=forms.CharField(widget=forms.NumberInput)
     # age=forms.IntegerField()
     # weight=forms.FloatField()
     # balance=forms.DecimalField()
-    # check=forms.BooleanField()
-    # birthday=forms.DateField()
-    # appointment=forms.DateTimeField()
-    # CHOICES=[('S','Small'),('M','Medium'),('L','Large')]
-    # size=forms.ChoiceField(choices=CHOICES)
-    # MEAL=[('P','Peperoni'),('M','Mashroom'),('B','Beef')]
-    # pizza=forms.MultipleChoiceField(choices=MEAL)
+    check=forms.BooleanField()
+    birthday=forms.DateField(widget=forms.DateInput(attrs={'type':'date'}))
+    appointment=forms.CharField(widget=forms.DateInput(attrs={'type':'datetime-local'}))
+    CHOICES=[('S','Small'),('M','Medium'),('L','Large')]
+    size=forms.ChoiceField(choices=CHOICES,widget=forms.RadioSelect)
+    MEAL=[('P','Peperoni'),('M','Mashroom'),('B','Beef')]
+    pizza=forms.MultipleChoiceField(choices=MEAL,widget=forms.CheckboxSelectMultiple)
