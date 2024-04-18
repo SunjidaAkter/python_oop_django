6fc5d55efcf908cf84f4722c32748b10d8480525
SunjidaAkter <sanjidabintesayid7@gmail.com>
2024-04-18 17:15:14 +0600

    mission accomplished 🔥🚀🔥

--- SoftwareDevelopmentProject/django/room/working_with_models/forms_in_django/fifth_project/first_app/forms.py
+++ SoftwareDevelopmentProject/django/room/working_with_models/forms_in_django/fifth_project/first_app/forms.py
@@ -2,4 +2,15 @@ from django import forms#form api
 
 class contactForm(forms.Form):
     name=forms.CharField(label="User name")
-    email=forms.EmailField()
+    file=forms.FileField()
+    # email=forms.EmailField(label="User email")
+    # age=forms.IntegerField()
+    # weight=forms.FloatField()
+    # balance=forms.DecimalField()
+    # check=forms.BooleanField()
+    # birthday=forms.DateField()
+    # appointment=forms.DateTimeField()
+    # CHOICES=[('S','Small'),('M','Medium'),('L','Large')]
+    # size=forms.ChoiceField(choices=CHOICES)
+    # MEAL=[('P','Peperoni'),('M','Mashroom'),('B','Beef')]
+    # pizza=forms.MultipleChoiceField(choices=MEAL)
