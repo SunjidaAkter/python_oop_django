836366846cbf7ea7c47caa2873d5a46827ac2d1d
SunjidaAkter <sanjidabintesayid7@gmail.com>
2024-05-01 18:35:18 +0600

    mission accomplished 🔥🚀🔥

--- SoftwareDevelopmentProject/django/room/authentication_authorization_and_class_based_view/authentication_vs_authorization/eighth_project/first_app/forms.py
+++ SoftwareDevelopmentProject/django/room/authentication_authorization_and_class_based_view/authentication_vs_authorization/eighth_project/first_app/forms.py
@@ -1,7 +1,8 @@
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
-
+from django import forms
 class RegisterForm(UserCreationForm):
+    first_name = forms.CharField(widget=forms.TextInput(attrs={'id':'required'}))
     class Meta:
         model=User
         fields=['username', 'email', 'first_name', 'last_name']
--- SoftwareDevelopmentProject/django/room/authentication_authorization_and_class_based_view/authentication_vs_authorization/eighth_project/first_app/views.py
+++ SoftwareDevelopmentProject/django/room/authentication_authorization_and_class_based_view/authentication_vs_authorization/eighth_project/first_app/views.py
@@ -2,5 +2,10 @@ from django.shortcuts import render
 from .forms import RegisterForm
 # Create your views here.
 def home(request):
-    form=RegisterForm()
+    form=RegisterForm()    
+    if request.method == 'POST':
+        form=RegisterForm(request.POST)
+        if form.is_valid():
+            form.save(commit=False)
+        
     return render(request, 'home.html',{'form':form})
