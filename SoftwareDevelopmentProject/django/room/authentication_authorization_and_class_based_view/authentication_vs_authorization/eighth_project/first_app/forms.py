1ab32395b3b423dac30a9147b014f98768977d42
SunjidaAkter <sanjidabintesayid7@gmail.com>
2024-05-02 17:19:18 +0600

    mission accomplished 🔥🚀🔥

--- SoftwareDevelopmentProject/django/room/authentication_authorization_and_class_based_view/authentication_vs_authorization/eighth_project/first_app/forms.py
+++ SoftwareDevelopmentProject/django/room/authentication_authorization_and_class_based_view/authentication_vs_authorization/eighth_project/first_app/forms.py
@@ -3,6 +3,8 @@ from django.contrib.auth.forms import UserCreationForm
 from django import forms
 class RegisterForm(UserCreationForm):
     first_name = forms.CharField(widget=forms.TextInput(attrs={'id':'required'}))
+    last_name = forms.CharField(widget=forms.TextInput(attrs={'id':'required'}))
+    email = forms.CharField(widget=forms.EmailInput(attrs={'id':'required'}))
     class Meta:
         model=User
         fields=['username', 'email', 'first_name', 'last_name']
--- SoftwareDevelopmentProject/django/room/authentication_authorization_and_class_based_view/authentication_vs_authorization/eighth_project/first_app/views.py
+++ SoftwareDevelopmentProject/django/room/authentication_authorization_and_class_based_view/authentication_vs_authorization/eighth_project/first_app/views.py
@@ -1,11 +1,37 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from .forms import RegisterForm
+from django.contrib import messages
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import authenticate,login,logout
 # Create your views here.
 def home(request):
+    return render(request,'./home.html')
+def signup(request):
     form=RegisterForm()    
     if request.method == 'POST':
         form=RegisterForm(request.POST)
         if form.is_valid():
+            messages.success(request,'Account created successfully')
+            # messages.warning(request,'Warning')
+            # messages.info(request,'Information')
             form.save(commit=False)
-        
+            print(form.cleaned_data)
     return render(request, 'home.html',{'form':form})
+
+def login(request):
+    if request.method == 'POST':
+        form=AuthenticationForm(request=request,data=request.POST)
+        if form.is_valid():
+            name=form.cleaned_data['username']
+            userpass=form.cleaned_data['password']
+            user=authenticate(username=name,password=userpass,userpass=userpass)
+            if user is not None:
+                login(request,user)
+                return redirect('profile')
+    else:
+        form=AuthenticationForm() 
+        return render(request,'./login.html',{'form':form}) 
+
+
+def profile(request):
+    return render(request,'./profile.html')          
