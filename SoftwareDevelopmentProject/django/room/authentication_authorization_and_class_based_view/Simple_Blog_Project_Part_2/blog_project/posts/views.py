e016ef84e67cc5f87d171a16be995d39e9e231af
SunjidaAkter <sanjidabintesayid7@gmail.com>
2024-05-03 19:22:38 +0600

    mission accomplished 🔥🚀🔥

---
+++ SoftwareDevelopmentProject/django/room/authentication_authorization_and_class_based_view/Simple_Blog_Project_Part_2/blog_project/posts/views.py
@@ -0,0 +1,32 @@
+from django.shortcuts import render,redirect
+from . import forms
+from . import models
+# Create your views here.
+def add_post(request):
+    post_form = forms.PostForm()
+    if request.method == "POST":
+        post_form = forms.PostForm(request.POST)  # Bind POST data to the form
+        print(post_form.is_valid())  # Check if form is valid
+        if post_form.is_valid():
+            post_form.save()
+            return redirect('homepage')
+        else:
+            print(post_form.errors)  # Print form errors for debugging
+    return render(request, 'add_post.html', {'form': post_form})
+def edit_post(request,id):
+    post=models.Post.objects.get(pk=id)
+    post_form = forms.PostForm(instance=post)
+    if request.method == "POST":
+        post_form = forms.PostForm(request.POST,instance=post)  # Bind POST data to the form
+        print(post_form.is_valid())  # Check if form is valid
+        if post_form.is_valid():
+            post_form.save()
+            return redirect('homepage')
+        else:
+            print(post_form.errors)  # Print form errors for debugging
+    return render(request, 'add_post.html', {'form': post_form})
+
+def delete_post(request, id):
+    post = models.Post.objects.get(pk=id)
+    post.delete()
+    return redirect('homepage')
