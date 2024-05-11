9c35cfa80fbca05a75287bda93a2a5f5388ba9ee
SunjidaAkter <sanjidabintesayid7@gmail.com>
2024-05-11 15:08:10 +0600

    mission accomplished 🔥🚀🔥

--- SoftwareDevelopmentProject/django/room/authentication_authorization_and_class_based_view/class_based_view/simple_blog_project_part_3/blog_project/blog_project/urls.py
+++ SoftwareDevelopmentProject/django/room/authentication_authorization_and_class_based_view/class_based_view/simple_blog_project_part_3/blog_project/blog_project/urls.py
@@ -2,6 +2,8 @@
 from django.contrib import admin
 from django.urls import path,include
 from .views import home
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -11,3 +13,4 @@ urlpatterns = [
     path('author/', include('author.urls')),
     path('post/', include('posts.urls')),
 ]
+urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
