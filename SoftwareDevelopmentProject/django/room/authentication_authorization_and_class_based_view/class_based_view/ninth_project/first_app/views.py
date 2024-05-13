ad926b96ae7e9541afdf5bda0e1fb94efeb9430d
SunjidaAkter <sanjidabintesayid7@gmail.com>
2024-05-13 23:58:50 +0600

    mission accomplished 🔥🚀🔥

---
+++ SoftwareDevelopmentProject/django/room/authentication_authorization_and_class_based_view/class_based_view/ninth_project/first_app/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+from datetime import datetime,timedelta
+# Create your views here.
+def home(request):
+    response = render(request, 'home.html')
+    response.set_cookie('name','rahim')
+    # response.set_cookie('name','karim',max_age=10)#10 seconds
+    response.set_cookie('name','karim',expires=datetime.utcnow()+timedelta(day=7))#10 seconds
+    return response
+
+def get_cookies(request):
+    name=request.COOKIES.get('name')
+    return render(request, 'get_cookies.html', {'name':name})
+
+def delete_cookies(request):
+    response = render(request, 'delete_cookies.html')
+    response.delete_cookie('name')
+    return response
