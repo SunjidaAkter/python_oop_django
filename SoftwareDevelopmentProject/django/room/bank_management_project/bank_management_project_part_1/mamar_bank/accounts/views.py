16c4c7e063c8a15ab6aac1d0fabec1d771f8ee50
SunjidaAkter <sanjidabintesayid7@gmail.com>
2024-06-01 12:46:35 +0600

    mission accomplished 🔥🚀🔥

--- SoftwareDevelopmentProject/django/room/bank_management_project/bank_management_project_part_1/mamar_bank/accounts/views.py
+++ SoftwareDevelopmentProject/django/room/bank_management_project/bank_management_project_part_1/mamar_bank/accounts/views.py
@@ -4,6 +4,7 @@ from django.contrib.auth import login ,logout
 from .forms import UserRegistrationForm,UserUpdateForm
 from django.urls import reverse_lazy
 from django.contrib.auth.views import LoginView,LogoutView
+from django.contrib import messages
 # Create your views here.
 class UserRegistrationView(FormView):
     template_name='accounts/user_registration.html'
@@ -13,17 +14,20 @@ class UserRegistrationView(FormView):
     def form_valid(self, form):
         user=form.save()
         login(self.request,user)
+        messages.success(self.request,'Successfully Registered')
         return super().form_valid(form)
 
 class UserLoginView(LoginView):
     template_name='accounts/user_login.html'
     def get_success_url(self):
+        messages.success(self.request,'Successfully Logged in')
         return reverse_lazy('home')
 
 class UserLogoutView(LogoutView):
     def get_success_url(self):
         if self.request.user.is_authenticated:
             logout(self.request)
+            messages.success(self.request, 'You have successfully logged out')
         return reverse_lazy('home')
     
 class UserBankAccountUpdateView(View):
@@ -37,6 +41,7 @@ class UserBankAccountUpdateView(View):
         form = UserUpdateForm(request.POST, instance=request.user)
         if form.is_valid():
             form.save()
+            messages.success(self.request,'Bank Account Updated Successfully')
             return redirect('profile')  # Redirect to the user's profile page
         return render(request, self.template_name, {'form': form})
         
