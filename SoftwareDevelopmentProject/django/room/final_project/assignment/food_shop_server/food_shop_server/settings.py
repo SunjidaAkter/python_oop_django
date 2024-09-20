823dce9be8ba92c003273c417562aa21f6d0721c
SunjidaAkter <sanjidabintesayid7@gmail.com>
2024-09-20 19:32:31 +0600

    mission accomplished 🔥🚀🔥

--- SoftwareDevelopmentProject/django/room/final_project/assignment/food_shop_server/food_shop_server/settings.py
+++ SoftwareDevelopmentProject/django/room/final_project/assignment/food_shop_server/food_shop_server/settings.py
@@ -88,18 +88,18 @@ WSGI_APPLICATION = 'food_shop_server.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
-    'default': dj_database_url.config(
-        default='postgresql://food_shop_server_user:7oAXdCehS35z7ysqBqC2aTylvTv05kE7@dpg-cr08a2lumphs7392bgdg-a.oregon-postgres.render.com/food_shop_server',
-    )
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
+
+# DATABASES = {
+#     'default': dj_database_url.config(
+#         default='postgresql://food_shop_server_user:7oAXdCehS35z7ysqBqC2aTylvTv05kE7@dpg-cr08a2lumphs7392bgdg-a.oregon-postgres.render.com/food_shop_server',
+#     )
+# }
 
 REST_FRAMEWORK = {
     'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
